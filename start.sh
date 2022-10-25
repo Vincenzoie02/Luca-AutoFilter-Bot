7f52da225ccb99a92bbb12c6ee03dc04c129a4eb
Vincenzoie02 <97029263+Vincenzoie02@users.noreply.github.com>
2022-10-25 16:05:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/Vincenzoie02/Luca-AutoFilter-Bot.git /Luca-AutoFilter-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Luca-AutoFilter-Bot
 fi
-cd /EvaMaria
+cd /Luca-AutoFilter-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
